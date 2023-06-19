f154666fd421377a572ebe238683ae73e39148b3
iTzMiNOS <amirerf1998@gmail.com>
2023-06-19 21:38:00 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Simple-Entries-Form';
+  objArray: Array<any> = [];
+  tmpid: number = this.objArray.length + 1;
+  tmpname: string = '';
+  tmpemail: string = '';
+  tmpadd: string = '';
+  addItem(){
+    this.objArray.push({id:this.objArray.length + 1,name:this.tmpname,email:this.tmpemail,adres:this.tmpadd});
+  }
+  deleteItem(index:number){
+    this.objArray.splice(index,1);
+  }
 }
+  
